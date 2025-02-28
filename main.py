79a7e897e260a618993d649815019bf950c4d520
Erema93 <eremenkojack1993@gmail.com>
2025-02-28 22:59:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ schedule.every().day.at("22:00").do(send_tales)
 while True:
     schedule.run_pending()
     time.sleep(60)
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render выдаёт порт
+    app.run(host="0.0.0.0", port=port)
